a7c8606a1a68a74cbc8f1fa1ca76b56c12cfde04
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-04-28 14:57:19 -0500

    added open/close/eventlistner for both and escape button

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+'use strict';
+
+//selecting different classes from html
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btsOpenModal = document.querySelectorAll('.show-modal');
+
+//creating functions for each open and close modal for resuse
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+//adding click eventlistner for both open and close modal
+for (let i=0; i<btsOpenModal.length; i++)
+btsOpenModal[i].addEventListener('click',openModal)
+overlay.addEventListener('click',openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//adding an event listner when Escape button is hit to close the modal
+
+document.addEventListener('keydown', function(e){
+ if (e.key ==='Escape' && !modal.classList.contains('hidden')){
+    closeModal();
+ }
+})
